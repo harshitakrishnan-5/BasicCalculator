6cdbff5b67063646f4978f233380ffef11b9747f
Harshita.Jujar <01fe24ica051@kletech.ac.in>
2025-10-30 17:22:08 +0530

    Addition & Subtraction

---
+++ Add-Sub.py
@@ -0,0 +1,14 @@
+# Program to perform addition and subtraction of two numbers
+
+# Input two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Perform addition and subtraction
+addition = num1 + num2
+subtraction = num1 - num2
+
+# Display the results
+print("\nResults:")
+print("Addition:", addition)
+print("Subtraction:", subtraction)
