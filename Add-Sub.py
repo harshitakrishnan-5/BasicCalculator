4dc0f2f5928c7bf405b2631a4e2d4d9cd9a6d02b
harshita.jain <01fe24ica040@kletech.ac.in>
2025-10-30 22:13:17 +0530

    Modified with multiplication and division

--- Add-Sub.py
+++ Add-Sub.py
@@ -7,8 +7,12 @@ num2 = float(input("Enter the second number: "))
 # Perform addition and subtraction
 addition = num1 + num2
 subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
 
 # Display the results
 print("\nResults:")
 print("Addition:", addition)
 print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division: ", division)
